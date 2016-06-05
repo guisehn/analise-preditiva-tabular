71319a9af5d178afe204e69e019835f35fa53de6
Guilherme Sehn <ghsehn@gmail.com>
2016-06-05 19:19:54 -0300

    fix follow set on productions with multiple symbols

--- js/follow-set-finder.js
+++ js/follow-set-finder.js
@@ -15,38 +15,40 @@ function getFollowSet(grammar, symbol, history) {
   if (symbol === grammar.startSymbol) {
     followSet.push('$')
   }
-  
+
   _.forEach(grammar.productionSet, (rightSide, leftSide) => {
     _.forEach(rightSide, production => {
-      var symbolIndex = production.indexOf(symbol)
+      var positionsOfSymbol = getSymbolPositions(production, symbol)
 
-      if (symbolIndex === -1) {
+      if (!positionsOfSymbol.length) {
         return
       }
-      
-      for (var i = symbolIndex; i < production.length; i++) {
-        var next = production[i + 1]
-        
-        // se é final da produção, inclui follow do lado esquerdo
-        if (next === undefined) {
-          // checagem para evitar recursão infinita
-          if (!_.includes(history, leftSide)) {
-            followSet = followSet.concat(getFollowSet(grammar, leftSide, history))
+
+      positionsOfSymbol.forEach(function(symbolPosition) {
+        for (var i = symbolPosition; i < production.length; i++) {
+          var next = production[i + 1]
+
+          // se é final da produção, inclui follow do lado esquerdo
+          if (next === undefined) {
+            // checagem para evitar recursão infinita
+            if (!_.includes(history, leftSide)) {
+              followSet = followSet.concat(getFollowSet(grammar, leftSide, history))
+            }
+
+            break
           }
 
-          break
-        }
+          // adiciona first do próximo símbolo ao follow set
+          var first = _.map(FirstSetFinder.getFirstSet(grammar, next), 'symbol')
+          followSet = followSet.concat(first.filter(s => s !== ''))
 
-        // adiciona first do próximo símbolo ao follow set
-        var first = _.map(FirstSetFinder.getFirstSet(grammar, next), 'symbol')
-        followSet = followSet.concat(first.filter(s => s !== ''))
-
-        // testa próximo símbolo apenas se conjunto first
-        // possui sentença vazia
-        if (!_.includes(first, '')) {
-          break
+          // testa próximo símbolo apenas se conjunto first
+          // possui sentença vazia
+          if (!_.includes(first, '')) {
+            break
+          }
         }
-      }
+      })
     })
   })
 
@@ -60,7 +62,7 @@ function getSymbolPositions(stringProduction, symbol){
         indexes.push(i);
       }
   }
-  return [indexes]
+  return indexes
 }
 
 function getFollowSets(grammar) {
